c5b4006297c82422b0c0a2a176c47da01dda2258
Jasper Haggenburg <jasperh@q42.nl>
2015-01-21 14:51:03 +0100

    moved js to seperate file

--- app/assets/javascripts/rails_admin_json_editor/rails_admin_json_editor.js
+++ app/assets/javascripts/rails_admin_json_editor/rails_admin_json_editor.js
@@ -1,1 +1,42 @@
 //= require rails_admin_json_editor/vue.0.11.4
+
+$(document).on('rails_admin.dom_ready', function() {
+  // TODO: Make this possible for multiple instances
+
+  var recordPickers = {};
+
+  var componentsVM = new Vue({
+    el: '[ref=components_json]',
+    data: {
+      components: $('[ref=components_json]').data('json')
+    },
+    methods: {
+      addComponent: function(e) {
+        e.preventDefault();
+        var type = e.target.getAttribute('component-type');
+        this.components.push({ type:type, props:{} })
+      },
+      onChangePicker: function(e, index, fieldName) {
+        var el = e.target;
+        var value = el.options[el.selectedIndex].getAttribute('data-json');
+        var json = JSON.parse(value);
+
+        var props = {};
+        props[fieldName] = json;
+
+        var data = $.extend({}, componentsVM.components[index], {
+          props: props
+        });
+
+        componentsVM.components.$set(index, data)
+      },
+      pickerOptionIsSelected: function(component, fieldName, recordLabel, recordName) {
+        return component.$data
+          && component.$data.props
+          && component.$data.props[fieldName]
+          && component.$data.props[fieldName][recordLabel] == recordName
+          && component.$data.props[fieldName][recordLabel] == recordName;
+      }
+    }
+  });
+});
---
+++ config/initializers/assets.rb
@@ -0,0 +1,1 @@
+Rails.application.config.assets.precompile += %w( rails_admin_json_editor/rails_admin_json_editor.js )
