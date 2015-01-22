020dbdc36419f3f4fe01c6dad838e36a1493414c
Jasper Haggenburg <jasperh@q42.nl>
2015-01-22 09:53:52 +0100

    toggle json and components

--- app/assets/javascripts/rails_admin_json_editor/rails_admin_json_editor.js
+++ app/assets/javascripts/rails_admin_json_editor/rails_admin_json_editor.js
@@ -4,18 +4,26 @@
 $(document).on('rails_admin.dom_ready', function() {
   // TODO: Make this possible for multiple instances
 
-  var cmsDefaults = { expanded: true };
-
+  // Get data
   var data = $('[ref=json-editor]').data('json');
   if(!data) {
     data = { components: [] };
   }
 
+  // Re-initialize temporary vars
+  data.tmp = { showJson: false };
+
+  // Re-initialMake sure all components have their (possibly new) defaults
+  var componentTmpDefaults = { expanded: true };
   data.components = _.map(data.components, function(component) {
-    if(!component.cms) component.cms = _.clone(cmsDefaults);
+    component.tmp = _.clone(componentTmpDefaults);
     return component;
   });
 
+  // Make sure we let Vue update the json-field, but do it here to prevent data-loss when js is disabled
+  $('[ref=json-editor] [v-model^="$root.$data"]').val('');
+
+  // Let's go
   var componentsVM = new Vue({
     el: '[ref=json-editor]',
     data: data,
@@ -26,7 +34,7 @@ $(document).on('rails_admin.dom_ready', function() {
         this.components.push({
           type: type,
           props: {},
-          cms: _.clone(cmsDefaults)
+          tmp: _.clone(componentTmpDefaults)
         })
       },
       removeComponent: function(index) {
